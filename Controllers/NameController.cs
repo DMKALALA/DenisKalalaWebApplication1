26533555be80adf4094c1a6f0a0b4996c92f04bd
DMKALALA <96592192+DMKALALA@users.noreply.github.com>
2024-11-03 15:21:16 -0500

    Added NameController to return name in JSON

---
+++ Controllers/NameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DenisKalalaWebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            var name = new { FirstName = "Denis", LastName = "Kalala" };
+            return Ok(name);
+        }
+    }
+}
